f8597fc7bc28e8a9415ed05722aa7bd4b3152362
Lara Sanders <larasanders@gmail.com>
2018-07-30 23:44:30 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,9 +14,9 @@ end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
+    # def set_product
+    #   @product = Product.find(params[:id])
+    # end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
