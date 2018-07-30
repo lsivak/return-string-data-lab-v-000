a8afdb8124155cc4590ac3f810e7b2a4e4154c61
Lara Sanders <larasanders@gmail.com>
2018-07-30 23:38:33 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180730233618) do
+ActiveRecord::Schema.define(version: 20180730233815) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "name"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20180730233618) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "description"
+    t.integer  "inventory"
   end
 
 end
