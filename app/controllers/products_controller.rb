8d2f704b61fa676048b782504afe0fcd234654bf
Lara Sanders <larasanders@gmail.com>
2018-07-31 00:05:43 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
   def create
   @product = Product.create(product_params)
   @product.save
-  redirect_to product_path(@product)
+  redirect_to products_path(@product)
 end
 
 def index
